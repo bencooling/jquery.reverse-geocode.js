8676574636fc5f8812726ea7fac2cc58c69b4c26
Ben Cooling <bcooling@surfstitch.com>
2014-05-01 17:35:34 +1000

    Fleshing out logic
    TODO: Add DOM manipulation based on response

--- jquery.reverse-geocode.js
+++ jquery.reverse-geocode.js
@@ -3,13 +3,16 @@
  *
  * Version: 0.1
  *
- * Requires: jQuery
+ * Requires:
+ * jQuery
+ * google-maps-v3 (https://maps.googleapis.com/maps/api/js?sensor=true)
+ * 
  */
 
 (function (factory) {
     if ( typeof define === 'function' && define.amd ) {
         // AMD. Register as an anonymous module.
-        define(['jquery'], factory);
+        define(['jquery', 'google-maps-v3'], factory);
     } else if (typeof exports === 'object') {
         // Node/CommonJS style for Browserify
         module.exports = factory;
@@ -19,16 +22,44 @@
     }
 }(function ($) {
 
-  $.fn.reverseGeocode = function(settings)
+  $.fn.reverseGeocode = function(type)
   {
-    // plugin here
 
+    var geocoder = new google.maps.Geocoder();
 
-    settings = $.extend({}, $.fn.jScrollPane.defaults, settings);
-  }
+     // requires navigator enabled browsers
+    if(!navigator.geolocation) 
+      return;
 
-  $.fn.reverseGeocode.defaults = {
+    navigator.geolocation.getCurrentPosition(function(pos) {
+      
+      var latlng = new google.maps.LatLng(pos.coords.latitude,pos.coords.longitude);
+      
+      geocoder.geocode({'latLng': latlng}, function(results, status) {
+        
+        var result = results[0]
+            ,city
+            ,state
+            ,component
+            ;
 
-  }
+        if (status == google.maps.GeocoderStatus.OK) {
+          
+          for(var i=0, len=result.address_components.length; i<len; i++) {
+            var component = result.address_components[i];
+            if(component.types.indexOf("locality") >= 0) 
+              city = component.long_name;
+            if(component.types.indexOf("administrative_area_level_1") >= 0) 
+              state = component.long_name;
+          }
+
+        }
+
+      });
+        
+    }
 
 }));
+
+https://maps.googleapis.com/maps/api/js?sensor=true
+
